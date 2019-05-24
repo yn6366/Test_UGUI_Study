cee7fa9de582981ac2ff2f0f498c3b46d731b3a5
unknown <yn6366@gmail.com>
2019-05-24 18:26:10 +0900

    ui Study

---
+++ Assets/Scripts/TestGUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGUI : MonoBehaviour
+{
+    public UIButton btn;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.btn.onClick.Add(new EventDelegate(() =>
+        {
+            Debug.Log("버튼 클릭");
+            this.btn.GetComponent<UISprite>().spriteName = "btn_start_f";
+            this.btn.normalSprite = "btn_start_f";
+        }));    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TestUGUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+
+public class TestUGUI : MonoBehaviour
+{
+    public Button button;
+    public Texture2D texture2;
+    public SpriteAtlas atlas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.button.onClick.AddListener(() =>
+        {
+            Debug.Log("버튼 활성화");
+            //var img = this.button.GetComponent<Image>();
+
+            ////Rect rect = new Rect(0, 0, texture.width, texture.height);
+            ////sprite.GetComponent<SpriteRenderer>().sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+
+            //Rect rect = new Rect(0,0, texture2.width , texture2.height);
+            //img.sprite = Sprite.Create(this.texture2, rect, new Vector3(0, 5f, 0.5f));
+
+            this.atlas.GetSprite("btn_start_f");
+        });    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
